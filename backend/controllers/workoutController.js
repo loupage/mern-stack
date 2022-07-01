c2e47b888ccb160074c182e4823fd3bcde94012c
Lou Page <loupage@gmail.com>
2022-07-01 09:55:30 +0100

    Project Finisthed

--- backend/controllers/workoutController.js
+++ backend/controllers/workoutController.js
@@ -1,87 +1,103 @@
 const Workout = require('../models/workoutModel')
 const mongoose = require('mongoose')
 
-// GET all workouts
+// get all workouts
 const getWorkouts = async (req, res) => {
-    const workouts = await Workout.find({}).sort({createdAt: -1})
+  const workouts = await Workout.find({}).sort({ createdAt: -1 })
 
-    res.status(200).json(workouts)
+  res.status(200).json(workouts)
 }
 
-// GET a single workout
+// get a single workout
 const getWorkout = async (req, res) => {
-    const { id } = req.params
+  const { id } = req.params
 
-    if(!mongoose.Types.ObjectId.isValid(id)){
-        return res.status(404).json({error: 'No such workout'})
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    return res.status(404).json({ error: 'No such workout' })
+  }
 
-    }
+  const workout = await Workout.findById(id)
 
-    const workout = await Workout.findById(id)
+  if (!workout) {
+    return res.status(404).json({ error: 'No such workout' })
+  }
 
-    if (!workout) {
-        return res.status(400).json({error: 'No such workout'})
-    }
-
-    res.status(200).json(workout)
-
+  res.status(200).json(workout)
 }
 
-// CREATE a new workout
+// create a new workout
 const createWorkout = async (req, res) => {
-    const {title, load, reps} = req.body
-    // Add Doc to DB
-    try {
-        const workout = await Workout.create({title, load, reps})
-        res.status(200).json(workout)
-    } catch (error) {
-        res.status(400).json({error:error.message})
-    }
+  const { title, load, reps } = req.body
+
+  let emptyFields = []
+
+  if (!title) {
+    emptyFields.push('title')
+  }
+  if (!load) {
+    emptyFields.push('load')
+  }
+  if (!reps) {
+    emptyFields.push('reps')
+  }
+  if (emptyFields.length > 0) {
+    return res
+      .status(400)
+      .json({ error: 'Please fill in all fields', emptyFields })
+  }
+
+  // add to the database
+  try {
+    const workout = await Workout.create({ title, load, reps })
+    res.status(200).json(workout)
+  } catch (error) {
+    res.status(400).json({ error: error.message })
+  }
 }
 
-// DELETE a workout
+// delete a workout
 const deleteWorkout = async (req, res) => {
-    const { id } = req.params
+  const { id } = req.params
 
-    if(!mongoose.Types.ObjectId.isValid(id)){
-        return res.status(404).json({error: 'No such workout'})
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    return res.status(400).json({ error: 'No such workout' })
+  }
 
-    }
+  const workout = await Workout.findOneAndDelete({ _id: id })
 
-    const workout = await Workout.findOneAndDelete({_id: id})
+  if (!workout) {
+    return res.status(400).json({ error: 'No such workout' })
+  }
 
-    if (!workout) {
-        return res.status(400).json({error: 'No such workout'})
-    }
-
-    res.status(200).json(workout)
-
+  res.status(200).json(workout)
 }
 
-// UPDATE a workout
+// update a workout
 const updateWorkout = async (req, res) => {
-    const { id } = req.params
+  const { id } = req.params
 
-    if(!mongoose.Types.ObjectId.isValid(id)){
-        return res.status(404).json({error: 'No such workout'})
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    return res.status(400).json({ error: 'No such workout' })
+  }
 
+  const workout = await Workout.findOneAndUpdate(
+    { _id: id },
+    {
+      ...req.body,
     }
+  )
 
-    const workout = await Workout.findOneAndUpdate({_id: id}, {...req.body})
+  if (!workout) {
+    return res.status(400).json({ error: 'No such workout' })
+  }
 
-    if (!workout) {
-        return res.status(400).json({error: 'No such workout'})
-    }
-
-    res.status(200).json(workout)
-
+  res.status(200).json(workout)
 }
-
 
 module.exports = {
-    createWorkout,
-    getWorkouts,
-    getWorkout,
-    deleteWorkout,
-    updateWorkout
+  getWorkouts,
+  getWorkout,
+  createWorkout,
+  deleteWorkout,
+  updateWorkout,
 }
